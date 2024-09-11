f65a6c9fba1dffdbf9255fc2e1895d2278b70a92
Agure Ogonji <agurejoseph@gmail.com>
2024-09-11 04:59:03 +0300

    uploadThing to Bin

--- src/lib/uploadThing.ts
+++
@@ -1,9 +0,0 @@
-import {generateComponents} from '@uploadthing/react'
-import {generateReactHelpers} from '@uploadthing/react/hooks'
-
-
-// export const UploadButton = generateUploadButton<OurFileRouter>()
-// export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
-// export const Uploader = generateUploader<OurFileRouter>()
-export const {UploadButton, UploadDropzone, Uploader} = generateComponents<OurFileRouter>()
-export const {useUploadThing, uploadFiles} = generateReactHelpers<OurFileRouter>()
